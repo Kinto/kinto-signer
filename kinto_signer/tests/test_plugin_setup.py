9f260550a11a198d7554bddfa25e5e92c3386795
Mathieu Leplatre <mathieu@mozilla.com>
2016-04-13 21:35:56 +0200

    Add mock for heartbeat

--- kinto_signer/tests/test_plugin_setup.py
+++ kinto_signer/tests/test_plugin_setup.py
@@ -22,23 +22,27 @@ class HelloViewTest(BaseWebTest, unittest.TestCase):
 
 class HeartbeatTest(BaseWebTest, unittest.TestCase):
 
+    def setUp(self):
+        patch = mock.patch('kinto_signer.signer.autograph.requests')
+        self.mock = patch.start()
+        self.addCleanup(patch.stop)
+        self.signature = {"signature": "",
+                          "hash_algorithm": "",
+                          "signature_encoding": ""}
+        self.mock.post.return_value.json.return_value = [self.signature]
+
     def test_heartbeat_is_exposed(self):
         resp = self.app.get('/__heartbeat__')
         assert "signer" in resp.json
 
     def test_heartbeat_fails_if_unreachable(self):
-        patch = mock.patch('kinto_signer.signer.autograph.requests')
-        mocked = patch.start()
-        self.addCleanup(patch.stop)
-        mocked.post.side_effect = requests_exceptions.ConnectTimeout()
+        self.mock.post.side_effect = requests_exceptions.ConnectTimeout()
         resp = self.app.get('/__heartbeat__', status=503)
         assert resp.json["signer"] is False
 
     def test_heartbeat_fails_if_missing_attributes(self):
-        patch = mock.patch('kinto_signer.signer.autograph.requests')
-        mocked = patch.start()
-        self.addCleanup(patch.stop)
-
-        mocked.post.return_value.json.return_value = [{"signature": "bar"}]
+        invalid = self.signature.copy()
+        invalid.pop('signature')
+        self.mock.post.return_value.json.return_value = [invalid]
         resp = self.app.get('/__heartbeat__', status=503)
         assert resp.json["signer"] is False

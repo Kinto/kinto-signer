ac62bae18228c0a228576441a2b69325b91b7396
Mathieu Leplatre <mathieu@mozilla.com>
2016-06-28 10:12:48 +0200

    Raise configuration errors if resources are not configured correctly (ref #88)

--- kinto_signer/utils.py
+++ kinto_signer/utils.py
@@ -1,16 +1,28 @@
 from collections import OrderedDict
+
+from kinto.views import NameGenerator
+
 from pyramid.settings import aslist
+from pyramid.exceptions import ConfigurationError
 
 
 def parse_resources(raw_resources):
     resources = OrderedDict()
+
+    name_generator = NameGenerator()
+
     for res in aslist(raw_resources):
         error_msg = ("Resources should be defined as "
                      "'/buckets/<bid>/collections/<cid>;"
-                     "/buckets/<bid>/collections/<cid>'. Got %r" % res)
+                     "/buckets/<bid>/collections/<cid>' and "
+                     "separated with space or linebreaks. Got %r" % res)
         if ";" not in res:
-            raise ValueError(error_msg)
-        source, destination = res.split(';')
+            raise ConfigurationError(error_msg)
+
+        try:
+            source, destination = res.split(';')
+        except ValueError:
+            raise ConfigurationError(error_msg)
 
         def _get_resource(resource):
             parts = resource.split('/')
@@ -19,7 +31,11 @@ def parse_resources(raw_resources):
             elif len(parts) == 5:
                 _, _, bucket, _, collection = parts
             else:
-                raise ValueError(error_msg)
+                raise ConfigurationError(error_msg)
+            valid_ids = (name_generator.match(bucket) and
+                         name_generator.match(collection))
+            if not valid_ids:
+                raise ConfigurationError(error_msg)
             return {
                 'bucket': bucket,
                 'collection': collection

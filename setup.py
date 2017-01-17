b6d6cd869894d2159c8b1f0235d83c020fd43c3a
Mathieu Leplatre <mathieu@mozilla.com>
2017-01-17 11:52:55 +0100

    Back to development: 1.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ REQUIREMENTS = [
 ]
 
 setup(name='kinto-signer',
-      version='1.1.1',
+      version='1.2.0.dev0',
       description='Kinto signer',
       long_description=README,
       license='Apache License (2.0)',

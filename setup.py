dd8b72becae59c18a624920a0364c8c53cd3e0b6
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-03-17 18:14:17 -0400

    Back to development: 1.3.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ REQUIREMENTS = [
 ]
 
 setup(name='kinto-signer',
-      version='1.3.1',
+      version='1.3.2.dev0',
       description='Kinto signer',
       long_description=README,
       license='Apache License (2.0)',

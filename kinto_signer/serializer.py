ef36539658d38b4d962eb80daa0e4710301e68dd
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-03-17 16:43:00 +0100

    @glasserc review.

--- kinto_signer/serializer.py
+++ kinto_signer/serializer.py
@@ -3,7 +3,7 @@ import operator
 
 
 def canonical_json(records, last_modified):
-    records = filter(lambda r: r.get('deleted', False) is not True, records)
+    records = (r for r in records if not r.get('deleted', False))
     records = sorted(records, key=operator.itemgetter('id'))
 
     payload = {'data': records, 'last_modified': '%s' % last_modified}

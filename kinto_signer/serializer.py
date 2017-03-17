26ec71d194dd57cb5741ccc51b9d3894c3b67046
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-03-17 11:03:06 +0100

    Remove use of deepcopy.

--- kinto_signer/serializer.py
+++ kinto_signer/serializer.py
@@ -1,10 +1,8 @@
-import copy
 import json
 import operator
 
 
 def canonical_json(records, last_modified):
-    records = copy.deepcopy(records)
     records = filter(lambda r: r.get('deleted', False) is not True, records)
     records = sorted(records, key=operator.itemgetter('id'))
 

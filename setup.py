bc3c8c4499bc10a084255c81296da89c982003ac
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-10-30 10:32:32 +0100

    Back to development: 2.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ REQUIREMENTS = [
 ]
 
 setup(name='kinto-signer',
-      version='2.1.1',
+      version='2.2.0.dev0',
       description='Kinto signer',
       long_description=README,
       license='Apache License (2.0)',

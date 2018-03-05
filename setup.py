ed6be77a92b8e6021b0dbe770de2e9fdf5f3fa5d
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-03-05 12:57:49 -0500

    Explicitly document supported versions

--- setup.py
+++ setup.py
@@ -21,6 +21,10 @@ setup(name='kinto-signer',
       license='Apache License (2.0)',
       classifiers=[
           "Programming Language :: Python",
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: CPython',
           "Topic :: Internet :: WWW/HTTP",
           "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
           "License :: OSI Approved :: Apache Software License"

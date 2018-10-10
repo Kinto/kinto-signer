66f380d591e07ab12cb3fd7625a23e314093f85a
Mathieu Leplatre <mathieu@mozilla.com>
2018-10-10 18:21:25 +0200

    Back to development: 3.4.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ REQUIREMENTS = [
 ]
 
 setup(name='kinto-signer',
-      version='3.3.3',
+      version='3.4.0.dev0',
       description='Kinto signer',
       long_description=README,
       license='Apache License (2.0)',

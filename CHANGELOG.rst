daaf1a824a3b9de16f3660859e514ef8ccd11b35
Mathieu Leplatre <mathieu@mozilla.com>
2019-08-27 15:40:20 +0200

    Preparing release 5.1.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,7 +3,7 @@ Changelog
 
 This document describes changes between each past release.
 
-5.1.0 (unreleased)
+5.1.0 (2019-08-27)
 ------------------
 
 **New Features**
--- setup.py
+++ setup.py
@@ -4,36 +4,33 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with codecs.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
     README = f.read()
 
-REQUIREMENTS = [
-    'kinto>=12.0.1',
-    'boto3',
-    'ecdsa',
-    'requests-hawk',
-]
+REQUIREMENTS = ["kinto>=12.0.1", "boto3", "ecdsa", "requests-hawk"]
 
-setup(name='kinto-signer',
-      version='5.1.0.dev0',
-      description='Kinto signer',
-      long_description=README,
-      license='Apache License (2.0)',
-      classifiers=[
-          "Programming Language :: Python",
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: Implementation :: CPython',
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-          "License :: OSI Approved :: Apache Software License"
-      ],
-      keywords="web services",
-      author='Mozilla Services',
-      author_email='services-dev@mozilla.com',
-      url='',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=REQUIREMENTS)
+setup(
+    name="kinto-signer",
+    version="5.1.0",
+    description="Kinto signer",
+    long_description=README,
+    license="Apache License (2.0)",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    keywords="web services",
+    author="Mozilla Services",
+    author_email="services-dev@mozilla.com",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+)

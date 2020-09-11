12e2447e2a4684c6b86eac98f7f289e3e36d1aad
Mathieu Leplatre <mathieu@mozilla.com>
2020-09-11 14:04:06 +0200

    Adjust error message

--- kinto_signer/listeners.py
+++ kinto_signer/listeners.py
@@ -364,7 +364,9 @@ def prevent_float_value(event, resources):
         for k, v in d.items():
             path = f"{path}.{k}" if path else k
             if isinstance(v, float):
-                raise ValueError(f"'{path}' field contains float value " "(tip: use string)")
+                raise ValueError(
+                    f"'{path}' field contains float value (tip: use integer or string)"
+                )
             elif isinstance(v, dict):
                 scan(v, path)
 
